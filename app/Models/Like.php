9d14159c05385b4e26eaf9217a6cb8a68460e01c
kotaromiyake.github.io <miyakou31@gmail.com>
2023-08-18 21:18:48 +0900

    update

--- app/Models/Like.php
+++ app/Models/Like.php
@@ -10,4 +10,10 @@ class Like extends Model
     use HasFactory;
 
     public $timestamps = false;
+
+    //
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
--- resources/views/users/posts/contents/modals/likes.blade.php
+++ resources/views/users/posts/contents/modals/likes.blade.php
@@ -1,4 +1,4 @@
-<div class="modal fade" id="like-post-{{ $post->id }}">
+<div class="modal fade" id="like-post-{{ $post->user->id }}">
     <div class="modal-dialog">
         <div class="modal-content border-warning">
             <div class="modal-header border-warning">
@@ -9,7 +9,26 @@
             </div>
             <div class="modal-body">
                 <div class="mt-3">
-                   {{ $post->id }}
+                    @foreach ($post->likes as $like)
+                    <div class="row align-items-center mb-3">
+                      <div class="col-auto">
+                          <a href="{{ route('profile.show',$like->user->id) }}">
+                          @if ($like->user->avatar)
+                          <img src="{{ $like->user->avatar }}" alt="{{ $like->user->avatar }}" class="rounded-circle avatar-sm">
+
+                          @else
+                          <i class="fa-solid fa-circle-user text-secondary icon-sm"></i>
+
+                          @endif</a>
+                      </div>
+                      <div class="col ps-0 text-truncate">
+                          <a href="{{ route('profile.show',$like->user->id) }}"
+                              class="fw-bold text-dark text-decoration-none">{{  $like->user->name }}</a>
+
+                      </div>
+
+                    </div>
+                    @endforeach
                 </div>
             </div>
 
